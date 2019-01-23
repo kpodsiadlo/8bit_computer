72f29380fac1857cfa63b6d7fbc5550d8380d23f
Krzysztof Podsiadło <kpodsiadlo@gmail.com>
2019-01-23 21:13:20 +0100

    first run with limited IR

--- main.py
+++ main.py
@@ -5,32 +5,40 @@ class Display:
     def __init__(self):
         self.state = 0
 
-    def do(self):
+    def do_in(self):
         if clock.state == 1:
             if logic.display_IN == 1:
                 self.state = buss.state
-        print("Display: " + self.state.__str__())
-        
+                print("Display: " + self.state.__str__())
+
 
 class MAR:
     def __init__(self):
         self.state = 0
 
-    def do(self):
+    def do_in(self):
         if clock.state == 1:
             if logic.MAR_IN == 1:
                 self.state = buss.state
-        print("MAR: " + self.state.__str__())
+                print("MAR: " + self.state.__str__())
 
 
 class RAM:
     def __init__(self):
-            self.state = [0 for x in range(16)]
+        self.state = [0 for x in range(16)]
+        self.state[0] =  0b00011110
+        self.state[1] =  0b00101111
+        self.state[2] =  0b00110000
+        self.state[14] = 0b00000110
+        self.state[15] = 0b00001000
 
-    def do(self):
+    def do_in(self):
         if clock.state == 1:
-            if logic.RAM_IN == 1:
-                self.state[mar.state] = buss.state
+            if logic.RAM_OUT == 1:
+                buss.state = self.state[mar.state]
+
+    def do_out(self):
+        if clock.state == 1:
             if logic.RAM_OUT == 1:
                 buss.state = self.state[mar.state]
 
@@ -45,32 +53,35 @@ class ALU:
         if self.state > 255:
             flag_register.carry = 1
 
-
-    def do(self):
-            self.regA = regA.state
-            self.regb = regB.state
-            self.state = regA.state + regB.state
-            if logic.ALU_Substract == 1:
-                self.state = regA.state - regB.state
-            if self.state > 255:
-                flag_register.carry = 1
-            if logic.ALU_OUT == 1:
-                buss.state = self.state
+    def do_out(self):
+        self.regA = regA.state
+        self.regb = regB.state
+        self.state = regA.state + regB.state
+        if logic.ALU_Substract == 1:
+            self.state = regA.state - regB.state
+        if self.state > 255:
+            flag_register.carry = 1
+        if logic.ALU_OUT == 1:
+            buss.state = self.state
 
 
 class ProgramCounter:
     def __init__(self):
         self.state = 0
 
-    def do(self):
+    def increase(self):
         if clock.state == 1:
-            if logic.count_enable == 1:
+            if logic.PC_enable == 1:
                 self.state += 1
                 print("PC: " + self.state.__str__())
-            if logic.PC_OUT == 1:
-                buss.state = self.state
-            if logic.PC_Jump == 1:
-                self.state = buss.state
+
+    def do_in(self):
+        if logic.PC_Jump == 1:
+            self.state = buss.state
+
+    def do_out(self):
+        if logic.PC_OUT == 1:
+            buss.state = self.state
 
 
 class Logic:
@@ -81,7 +92,7 @@ class Logic:
         self.BregisterOUT = 0
         self.IregisterIN = 0
         self.IregisterOUT = 0
-        self.count_enable = 0
+        self.PC_enable = 0
         self.PC_OUT = 0
         self.PC_Jump = 0
         self.ALU_OUT = 0
@@ -117,76 +128,183 @@ class Clock:  # start(time interval in seconds) .stop
 
 class Buss:
     def __init__(self):
-        self.state = 0
+        self.state = None
+
+    def do(self):
+        self.state = None
 
 
 class RegisterA:
     def __init__(self):
         self.state = 0
 
-    def do(self):
+    def do_in(self):
         if clock.state == 1:
             if logic.AregisterIN == 1:
                 self.state = buss.state
+                print("RegA: " + self.state.__str__())
+
+    def do_out(self):
+        if clock.state == 1:
             if logic.AregisterOUT == 1:
                 buss.state = self.state
-        print("RegA: " + self.state.__str__())
+                print("RegA: " + self.state.__str__())
 
 
 class RegisterB:
     def __init__(self):
         self.state = 0b00000000
 
-    def do(self):
+    def do_in(self):
         if clock.state == 1:
             if logic.BregisterIN == 1:
                 self.state = buss.state
+                print("RegB: " + self.state.__str__())
+
+    def do_out(self):
+        if clock.state == 1:
             if logic.BregisterOUT == 1:
                 buss.state = self.state
-        print("RegB: " + self.state.__str__())
+                print("RegB: " + self.state.__str__())
 
 
 class InstructionRegister:
     def __init__(self):
         self.state = 0b00000000
+        self.higher_bits = 0
+        self.lower_bits = 0
 
-    def do(self):
+    def do_in(self):
         if clock.state == 1:
             if logic.IregisterIN == 1:
                 self.state = buss.state
+                self.split()
+                print("Instruction register: " + self.state.__str__())
+
+    def do_out(self):
+        if clock.state == 1:
             if logic.IregisterOUT == 1:
-                buss.state = self.state
-        print("RegB: " + self.state.__str__())
+                buss.state = self.lower_bits
+                print("Instruction register:: " + self.state.__str__())
+
+    def split(self):
+        self.higher_bits = self.state >> 4
+        print("IR_HIGHER:" + self.higher_bits.__str__())
+        self.lower_bits = self.state & 0b00001111
+        print("IR_LOWER: " + self.lower_bits.__str__())
 
 
-class FlagRegister():
+class FlagRegister:
     def __init__(self):
         self.carry = 0
 
+
+class InstructionCounter:
+    def __init__(self):
+        self.state = 0
+
+    def increase(self):
+        if clock.state == 1:
+            self.state += 1
+            if self.state > 5:
+                self.state = 0
+
+
+class Decoder:
+    def __init__(self):
+        self.do()
+
+    @staticmethod
+    def do():
+        print("T: " + ic.state.__str__())
+
+        logic.reset()
+        if ic.state == 0:  # Microinstruction 0 (Fetch)
+            logic.PC_OUT = 1
+            logic.MAR_IN = 1
+        if ic.state == 1:  # Microinstruction 1 (Fetch)
+            logic.RAM_OUT = 1
+            logic.IregisterIN = 1
+            logic.PC_enable = 1
+        if ic.state > 1:
+            execute()
+
+
+
 def do():
-    pc.do()
-    mar.do()
-    ram.do()
-    alu.do()
-    regA.do()
-    regB.do()
-    display.do()
+    buss.do()
+    decoder.do()
+
+    # outs
+
+    pc.do_out()
+    regA.do_out()
+    regB.do_out()
+    alu.do_out()
+    instruction_register.do_out()
+    ram.do_out()
+
+    # ins
+    pc.do_in()
+    regA.do_in()
+    regB.do_in()
+    instruction_register.do_in()
+    mar.do_in()
+    ram.do_in()
+    display.do_in()
+
+    # increase:
+
+    pc.increase()
+    ic.increase()
 
 
 def bprint(output):
-        print(format(output, '#010b'))
+    print(format(output, '#010b'))
 
 
-display = Display()
-mar = MAR()
-ram = RAM()
-buss = Buss()
-pc = ProgramCounter()
-logic = Logic()
 clock = Clock()
+pc = ProgramCounter()
+ic = InstructionCounter()
+logic = Logic()
+decoder = Decoder()
+buss = Buss()
 regA = RegisterA()
 regB = RegisterB()
-flag_register = FlagRegister()
 alu = ALU()
+instruction_register = InstructionRegister()
+
+flag_register = FlagRegister()
+mar = MAR()
+ram = RAM()
+display = Display()
+
 # clock.start(2)
-#### EMULACJA SZYNY BDZIE SIE MUSIALA ODBYWAC PRZEZ ODPALANIE TYLKO AKTUALNIE POTRZEBNYCH MODUŁÓW.
+
+
+def execute():
+    if instruction_register.higher_bits == 1:  ## lda
+        if ic.state == 2:
+            logic.IregisterOUT = 1
+            logic.MAR_IN = 1
+
+        if ic.state == 3:
+            logic.RAM_OUT = 1
+            logic.AregisterIN = 1
+
+    if instruction_register.higher_bits == 2:  ## add
+        if ic.state == 2:
+            logic.IregisterOUT = 1
+            logic.MAR_IN = 1
+        if ic.state == 3:
+            logic.RAM_OUT = 1
+            logic.BregisterIN = 1
+        if ic.state == 4:
+            logic.ALU_OUT = 1
+            logic.AregisterIN = 1
+
+
+    if instruction_register.higher_bits == 3: ## display
+        if ic.state == 2:
+            logic.AregisterOUT = 1
+            logic.display_IN = 1
