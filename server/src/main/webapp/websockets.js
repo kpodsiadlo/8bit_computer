da8b7fd152499cb004b35648ddbd8f3431de008e
kpodsiadlo <kpodsiadlo@gmail.com>
2021-03-03 18:22:22 +0100

    added update functions in websockets.js

--- server/src/main/webapp/websockets.js
+++ server/src/main/webapp/websockets.js
@@ -5,8 +5,34 @@ function onMessage(event) {
     console.log("Event received");
     let computerData = JSON.parse(event.data);
     console.log(computerData);
+    updateDisplays(computerData);
+    
+}
+
+function updateDisplays(computerData) {
+    updateMemoryAddress(computerData.memoryAddress)
     updateProgramCounterDisplay(computerData.programCounter);
+    updateMemoryContents(computerData.memoryContents);
+    updateInstructionRegister(computerData.instructionRegister);
+    updateMicroinstructionCounter(computerData.microinstructionCounter);
     updateRegisterADisplay(computerData.registerA);
+    updateRegisterBDisplay(computerData.registerB);
+    updateOutputDisplay(computerData.outputDisplay);
+}
+function updateMemoryAddress(data) {
+    document.getElementById("memory-address-display").value = data;
+}
+
+function updateMemoryContents(data) {
+    document.getElementById("memory-content-display").value = data;
+}
+
+function updateInstructionRegister (data) {
+    document.getElementById("instruction-register-display").value = data;
+}
+
+function updateMicroinstructionCounter(data) {
+    document.getElementById("microinstruction-counter-display").value = data;
 }
 
 function updateProgramCounterDisplay(data) {
@@ -17,6 +43,15 @@ function updateRegisterADisplay(data) {
     document.getElementById("register-a-display").value = data;
 }
 
+function updateRegisterBDisplay(data) {
+    document.getElementById("register-b-display").value = data;
+}
+
+function updateOutputDisplay(data){
+    document.getElementById("output-display").value = data;
+}
+
+
 function onProgramCounterIncrease() {
     var currentValue = parseInt(document.getElementById("program-counter-display").value);
     updateProgramCounterDisplay(currentValue + 1)
@@ -25,8 +60,8 @@ function onProgramCounterIncrease() {
 
 function sendUpdate() {
     console.log("sendUpdate");
-    var programCounter = parseInt(document.getElementById("program-counter-display").value);
-    var jsonMessage = {
+    let programCounter = parseInt(document.getElementById("program-counter-display").value);
+    let jsonMessage = {
         "programCounter": programCounter
     };
     console.log(jsonMessage);
