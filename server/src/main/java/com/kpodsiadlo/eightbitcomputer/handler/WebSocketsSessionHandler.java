d34fae71780eab5ac3ac95b1d08c88688ed5734b
kpodsiadlo <kpodsiadlo@gmail.com>
2021-04-20 23:47:11 +0200

    changed add_session behaviour to accept uuids

--- server/src/main/java/com/kpodsiadlo/eightbitcomputer/handler/WebSocketsSessionHandler.java
+++ server/src/main/java/com/kpodsiadlo/eightbitcomputer/handler/WebSocketsSessionHandler.java
@@ -3,27 +3,35 @@ package com.kpodsiadlo.eightbitcomputer.handler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ejb.Stateless;
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 import javax.websocket.MessageHandler;
 import javax.websocket.Session;
-import java.io.IOException;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 @ApplicationScoped
 public class WebSocketsSessionHandler implements MessageHandler {
 
-    private final Set<Session> sessions = new HashSet<>();
+    private final Map<String, Set<Session>> sessions = new HashMap<>();
     private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
 
     public void forwardMessage(String message, Session originSession) {
         sendToAllOtherSessions(message, originSession);
     }
 
+
     public void addSession(Session session) {
-        sessions.add(session);
+        Set<Session> commonSessions;
+        try {
+            commonSessions = this.sessions.get(session.getNegotiatedSubprotocol());
+            commonSessions.add(session);
+            sessions.put(session.getNegotiatedSubprotocol(), commonSessions);
+        } catch (NullPointerException e) {
+            Set<Session> newCommonSessionsSet = Set.of(session);
+            this.sessions.put(session.getNegotiatedSubprotocol(), newCommonSessionsSet);
+        }
     }
 
     public void removeSession(Session session) {
@@ -31,24 +39,24 @@ public class WebSocketsSessionHandler implements MessageHandler {
     }
 
     public void sendToSession(Session session, String message) {
-        try {
-            session.getBasicRemote().sendText(message);
-        } catch (IOException e) {
-            removeSession(session);
-            logger.error(e.getMessage());
-        }
+//        try {
+//            session.getBasicRemote().sendText(message);
+//        } catch (IOException e) {
+//            removeSession(session);
+//            logger.error(e.getMessage());
+//        }
     }
 
     public void sendToAllOtherSessions(String message, Session originSession) {
-        logger.debug("Sending: {}", message);
-        Set<Session> receivingSessions = new HashSet<>(sessions);
-        receivingSessions.remove(originSession);
-        receivingSessions.forEach(session -> sendToSession(session, message));
+//        logger.debug("Sending: {}", message);
+//        Set<Session> receivingSessions = new HashSet<>(sessions);
+//        receivingSessions.remove(originSession);
+//        receivingSessions.forEach(session -> sendToSession(session, message));
 
     }
 
     public void sendToAllSessions(String message) {
-        sessions.forEach(session -> sendToSession(session, message));
+//        sessions.forEach(session -> sendToSession(session, message));
     }
 
 
