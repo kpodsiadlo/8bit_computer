ae6c97950b488ecd2da3f7a221229bc14f42ffea
kpodsiadlo <kpodsiadlo@gmail.com>
2021-04-21 17:21:05 +0200

    websocket connection problems

--- engine/websockets_client.py
+++ engine/websockets_client.py
@@ -7,10 +7,9 @@ from message_processor import WebsocketMessageProcessor
 # uuidSignature = sys.argv[1]
 uuidSignature = uuid.uuid4()
 protocols = [str(uuidSignature)]
-print(str(protocols))
 message_processor = WebsocketMessageProcessor()
 
-uri = "ws://localhost:8080/server/computer "
+uri = "ws://localhost:8080/server/computer"
 
 
 async def producer_handler(websocket):
@@ -23,17 +22,17 @@ async def consumer_handler(websocket):
 
 
 async def handler():
-    async with websockets.connect(uri) as websocket:
+    async with websockets.connect(uri, subprotocols=protocols) as websocket:
         consumer_task = asyncio.ensure_future(
             consumer_handler(websocket))
-    producer_task = asyncio.ensure_future(
-        producer_handler(websocket))
-    done, pending = await asyncio.wait(
-        [consumer_task, producer_task],
-        return_when=asyncio.FIRST_COMPLETED,
-    )
-    for task in pending:
-        task.cancel()
+        producer_task = asyncio.ensure_future(
+            producer_handler(websocket))
+        done, pending = await asyncio.wait(
+            [consumer_task, producer_task],
+            return_when=asyncio.FIRST_COMPLETED,
+        )
+        for task in pending:
+            task.cancel()
 
 
 asyncio.get_event_loop().run_until_complete(handler())
