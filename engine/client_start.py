8180f886ca822ac3c52c2109b45d3787cb2719ac
kpodsiadlo <kpodsiadlo@gmail.com>
2021-05-13 01:34:12 +0200

    removed unnecessary client_start.py

--- engine/client_start.py
+++
@@ -1,6 +0,0 @@
-from engine.websocketclient.websocket_client import WebsocketClient
-
-clock_speed = 10  #Hz
-
-client = WebsocketClient(clock_speed=clock_speed, targetId = target_id)
-client.start()
