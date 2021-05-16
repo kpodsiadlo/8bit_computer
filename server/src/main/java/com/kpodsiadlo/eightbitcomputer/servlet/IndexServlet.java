222dea9c523bc80bc8458e1c40782bac5abb1bbe
kpodsiadlo <kpodsiadlo@gmail.com>
2021-05-17 00:08:52 +0200

    recaptcha working including forwarding back to recaptcha page

--- server/src/main/java/com/kpodsiadlo/eightbitcomputer/servlet/IndexServlet.java
+++ server/src/main/java/com/kpodsiadlo/eightbitcomputer/servlet/IndexServlet.java
@@ -6,12 +6,10 @@ import org.slf4j.LoggerFactory;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.UUID;
 
 @WebServlet("/index.html")
 public class IndexServlet extends HttpServlet {
@@ -19,6 +17,13 @@ public class IndexServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("login.html");
+        requestDispatcher.forward(req, resp);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req,
+                       HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("login.html");
         requestDispatcher.forward(req, resp);
     }
